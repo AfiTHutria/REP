361b64a0aa94d4dabbfab976db2d2187344fce42
AfiTHutria <afithutria10@gmail.com>
2025-10-22 20:59:58 -0500

    Segundo commit

--- frontend/react/src/presentation/components/navbar.jsx
+++ frontend/react/src/presentation/components/navbar.jsx
@@ -14,25 +14,25 @@ export default function navbar() {
         <nav className="Navegacion principal">
           <ul className="menu">
             <li>
-              <a href="facebook.com">Ver todo</a>
+              <a href="#">Ver todo</a>
             </li>
             <li>
               <a href="#">Nuevos</a>
             </li>
             <li>
-              <a href="">Superiores</a>
+              <a href="#">Superiores</a>
             </li>
             <li>
-              <a href="">Inferiores</a>
+              <a href="#">Inferiores</a>
             </li>
             <li>
-              <a href="">Enterizos</a>
+              <a href="#">Enterizos</a>
             </li>
             <li>
-              <a href="">Sets</a>
+              <a href="#">Sets</a>
             </li>
             <li>
-              <a href="">Bono regalo</a>
+              <a href="#">Bono regalo</a>
             </li>
           </ul>
         </nav>
@@ -41,13 +41,13 @@ export default function navbar() {
           <a href="#" className="icon-btn" aria-label="Buscar">
             <FiSearch />
           </a>
-          <a href="#" className="icon-btn" aria-label="Buscar">
+          <a href="#" className="icon-btn" aria-label="Usuario">
             <FiUser />
           </a>
-          <a href="#" className="icon-btn" aria-label="Buscar">
+          <a href="#" className="icon-btn" aria-label="Favoritos">
             <FiHeart />
           </a>
-          <a href="#" className="icon-btn cart" aria-label="Buscar">
+          <a href="#" className="icon-btn cart" aria-label="Carrito">
             <CgShoppingCart />
             <span className="badge" aria-label="Productos en carrito">
               1
